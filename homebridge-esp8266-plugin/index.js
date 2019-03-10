7f9c1566936a8363faf449fe4b8a3981659dd617
Mario Uher <uher.mario@gmail.com>
2019-03-10 16:30:51 +0100

    Get everything working

--- homebridge-esp8266-plugin/index.js
+++ homebridge-esp8266-plugin/index.js
@@ -1,4 +1,5 @@
 const { Service, Characteristic } = require('hap-nodejs')
+const http = require('http')
 
 class D1Mini {
   constructor (log, config) {
@@ -8,7 +9,31 @@ class D1Mini {
 
     this.switchService
       .getCharacteristic(Characteristic.On)
-      .on('set', (value, callback) => { this.log(`Active: ${value}`); callback() })
+      .on('get', this.getSwitchOnCharacteristic.bind(this))
+      .on('set', this.setSwitchOnCharacteristic.bind(this))
+
+    http
+      .request({
+        method: 'GET',
+        hostname: '10.0.0.34',
+        port: 80,
+        path: '/'
+      })
+      .on('response', (response) => {
+        let body = ''
+
+        response.on('data', (data) => {
+          body += data
+        })
+
+        response.on('end', () => {
+          this.switchService
+            .getCharacteristic(Characteristic.On)
+            .setValue(body === 'on')
+        })
+      })
+      .on('error', (error) => this.log.error(error.message))
+      .end()
   }
 
   getServices () {
@@ -21,14 +46,45 @@ class D1Mini {
   }
 
   getSwitchOnCharacteristic (callback) {
-    return callback(null, this.state)
+    this.log('getSwitchOnCharacteristic')
+
+    http
+      .request({
+        method: 'GET',
+        hostname: '10.0.0.34',
+        port: 80,
+        path: '/'
+      })
+      .on('response', (response) => {
+        let body = ''
+
+        response.on('data', (data) => {
+          body += data
+        })
+
+        response.on('end', () => {
+          callback(null, body === 'on')
+        })
+      })
+      .on('error', (error) => this.log.error(error.message))
+      .end()
   }
 
   setSwitchOnCharacteristic (value, callback) {
-    this.state = value
-    console.log('state set to', this.state)
+    this.log('setSwitchOnCharacteristic', value)
 
-    return callback()
+    http
+      .request({
+        method: 'POST',
+        hostname: '10.0.0.34',
+        port: 80,
+        path: value ? '/on' : '/off'
+      })
+      .on('response', (response) => {
+        callback()
+      })
+      .on('error', (error) => this.log.error(error.message))
+      .end()
   }
 }
 
