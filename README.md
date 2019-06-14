27e856f07b396f11c96f6cf16995780a6683064e
Mario Uher <uher.mario@gmail.com>
2019-06-14 12:46:37 +0200

    Implement MQTT

--- README.md
+++ README.md
@@ -5,25 +5,24 @@
 # homebridge-esp8266-example
 
 This is an example for building custom HomeKit appliances using
-[Homebridge](https://homebridge.io) and ESP8266-based microcontrollers. It uses
-[dns-sd](http://www.dns-sd.org) for easier service discovery.
-
-![](/dns-sd.png)
+[Homebridge](https://homebridge.io) and ESP8266-based micro-controllers. It uses
+[dns-sd](http://www.dns-sd.org) for service discovery and MQTT for communication
+between the Homebridge plugin and the ESP8266 micro-controller(s).
 
 ## [sketch](/sketch)
 
-This is an Arduino sketch for any ESP8266-based microcontroller which allows the
-built-in LED to be toggled on and off via a simple HTTP API:
+This is an Arduino sketch for any ESP8266-based micro-controller which allows the
+built-in LED to be toggled on and off via MQTT:
 
 ```
-curl http://10.0.0.26
+mosquitto_sub -t esp8266/led/status
 # => "on" or "off"
 
-curl -d on http://10.0.0.26
-# => "on"
+# Turn LED on
+mosquitto_pub -t esp8266/led/action -m on
 
-curl -d off http://10.0.0.26
-# => "off"
+# Turn LED off
+mosquitto_pub -t esp8266/led/action -m off
 ```
 
 ### Usage
@@ -32,8 +31,8 @@ Be sure to create a `sketch/config.h` based on `sketch/config.example.h` using
 your WiFi credentials. Either use the Arduino IDE or [something which builds
 upon the
 IDE](https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.vscode-arduino)
-to compile and flash this sketch onto the microcontroller. After booting and
-obtaining an IP the HTTP API should be ready and accepting your requests.
+to compile and flash this sketch onto the micro-controller. After booting and
+obtaining an IP, the ESP8266 searches for a MQTT broker and subscribes to it.
 
 ## [homebridge-esp8266-plugin](/homebridge-esp8266-plugin)
 
@@ -51,6 +50,7 @@ npm install
 npm start
 ```
 
-## TODOs
+# Links
 
-- [ ] Try out MQTT.
+- https://github.com/KhaosT/HAP-NodeJS/blob/81319b35d1588453cfcb1a823805643de7df74dc/lib/gen/HomeKitTypes.js
+- https://smarthome-blogger.de/tutorial/esp8266-mqtt-tutorial/
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,6 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #include "./config.h"
 
@@ -7,7 +8,17 @@
 #define LED_ON LOW
 #define LED_OFF HIGH
 
+#define MQTT_TOPIC_ACTION "esp8266/led/action"
+#define MQTT_TOPIC_STATUS "esp8266/led/status"
+
+// The interval in which the status is reported.
+#define MQTT_INTERVAL 5000
+
 char accessoryName[16] = {0};
+unsigned long lastLoop = 0;
+
+WiFiClient wifi;
+PubSubClient mqtt(wifi);
 
 void setup()
 {
@@ -23,6 +34,7 @@ void setup()
 
   setupWiFi();
   setupMDNS();
+  setupMQTT();
 }
 
 void setupWiFi()
@@ -55,9 +67,99 @@ void setupMDNS()
   {
     Serial.println("MDNS responder started");
   }
+
+  int mqttServices = MDNS.queryService("homebridge", "tcp");
+  if (mqttServices == 0)
+  {
+    Serial.println("No MQTT broker found!");
+  }
+  else
+  {
+    Serial.print("MQTT broker found at ");
+    Serial.println(MDNS.IP(0));
+  }
+}
+
+void setupMQTT()
+{
+  mqtt.setServer(MDNS.IP(0), 1883);
+  mqtt.setCallback(callback);
+}
+
+void connectMQTT()
+{
+  while (!mqtt.connected())
+  {
+    Serial.print("Connecting to MQTT broker, ");
+    if (!mqtt.connect(accessoryName))
+    {
+      Serial.print("failed with ");
+      Serial.print(mqtt.state());
+      Serial.println(", retrying in 5 seconds!");
+      delay(5000);
+    }
+    else
+    {
+      Serial.println("done, subscribed to ");
+      mqtt.subscribe(MQTT_TOPIC_ACTION);
+      Serial.println(MQTT_TOPIC_ACTION);
+    }
+  }
 }
 
 void loop()
 {
   MDNS.update();
+  connectMQTT();
+  mqtt.loop();
+  publishStatus();
+}
+
+const char *status()
+{
+  return digitalRead(LED_BUILTIN) == LED_ON ? "on" : "off";
+}
+
+void publishStatus()
+{
+  if (millis() - lastLoop >= MQTT_INTERVAL)
+  {
+    lastLoop += MQTT_INTERVAL;
+    mqtt.publish(MQTT_TOPIC_STATUS, status());
+  }
+}
+
+char *readPayload(byte *payload, unsigned int length)
+{
+  char output[length + 1];
+
+  for (int i = 0; i < length; i++)
+  {
+    output[i] = (char)payload[i];
+  }
+
+  output[length] = '\0';
+
+  return output;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+
+  char *msg = readPayload(payload, length);
+
+  Serial.print("Received message \"");
+  Serial.print(msg);
+  Serial.println("\"");
+
+  if (strcmp(msg, "on") == 0)
+  {
+    digitalWrite(LED_BUILTIN, LED_ON);
+    mqtt.publish(MQTT_TOPIC_STATUS, status());
+  }
+  else if (strcmp(msg, "off") == 0)
+  {
+    digitalWrite(LED_BUILTIN, LED_OFF);
+    mqtt.publish(MQTT_TOPIC_STATUS, status());
+  }
 }
